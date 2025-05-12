c53bd73c1e1b9be026576065a9dec848257b27f3
flowerbin <flowerbin@gmail.com>
2025-05-12 11:56:31 +0800

    Update sqlite.ts

--- src/utils/sqlite.ts
+++ src/utils/sqlite.ts
@@ -17,7 +17,8 @@ export const initDB = async () => {
         )`);
 };
 
-export const getDB = async () => {
+export const getDB =  = async () =>  => {
+    console.log('AESKEY:', AESKEY);
     return await open({
         filename: DB_FILE_PATH,
         driver: sqlite3.Database,
